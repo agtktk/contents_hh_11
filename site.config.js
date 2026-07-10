071b2d1a6af9221a3ee1de09fc94ab78c53728ea
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:53:22 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: 'kt정보이용료 희망티켓',
+  siteUrl: 'https://contents-hh-11.pages.dev',
+  siteDescription: 'kt정보이용료 공식 상담처. 올레 케이티 고객을 위한 실시간 정책 해결 및 안전하고 빠른 지원을 약속합니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
